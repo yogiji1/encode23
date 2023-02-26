49f7e331f0c276267cd479a8f6f213dc19807faa
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-02-26 07:34:39 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -38,4 +38,4 @@ class Config(object):
     UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "▢")
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
       # because, https://t.me/c/1494623325/5603
-    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", False)
+    SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", True)
