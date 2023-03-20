28c19e585b28bc8c9263584612d147bc5d778fdc
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-03-20 06:49:27 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -6,7 +6,7 @@ from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Encoder Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\nOwner : @TheBatmanShan"
+    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Encoder Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\nOwner : @cmd_rulf"
    
     ABS_TEXT = " Please don't be selfish."
     
@@ -47,7 +47,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @Linux_Repo"
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @Rulf_MLTB"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
