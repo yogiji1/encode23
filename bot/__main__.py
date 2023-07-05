2d0e3a3999e9e67d52e6d7fce6dcf5708784f134
Harshhacker0771 <122040332+Harshhacker0771@users.noreply.github.com>
2023-07-06 00:13:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -171,7 +171,7 @@ if __name__ == "__main__" :
     @app.on_message(filters.incoming & (filters.video | filters.document))
     async def help_message(app, message):
         if message.chat.id not in AUTH_USERS:
-            return await message.reply_text("You are not authorised to use this bot contact @cmd_rulf")
+            return await message.reply_text("You are not authorised to use this bot contact @PROFE07XH")
         query = await message.reply_text("Added to Queue ⏰...\nPlease be patient, Compress will start soon", quote=True)
         data.append(message)
         if len(data) == 1:
